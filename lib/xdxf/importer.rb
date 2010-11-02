345764ca272071034d0dd73aa475037c90389c47
Александр Ульяницкий <a.ulyanitsky@gmail.com>
2010-11-03 00:38:45 +0200

    Added logging and benchmark for importer

--- lib/xdxf/importer.rb
+++ lib/xdxf/importer.rb
@@ -28,33 +28,46 @@ module XDXF
       doc = Nokogiri.XML(xml)
      
       transaction do 
+        Dictionary.benchmark("Dictionary import", Logger::DEBUG, false) do
       
-        # Info regarding the dict type
-        dictionary_record = XDXF::Dictionary.create!(
-          :full_name => doc.css('full_name').first.content,
-          :description => doc.css('description').first.content,
-          :lang_from => doc.css('xdxf').first.attributes['lang_from'].value,
-          :lang_to   => doc.css('xdxf').first.attributes['lang_to'].value
-        )
+          # Info regarding the dict type
+          dictionary_record = Dictionary.create!(
+            :full_name => doc.css('full_name').first.content,
+            :description => doc.css('description').first.content,
+            :lang_from => doc.css('xdxf').first.attributes['lang_from'].value,
+            :lang_to   => doc.css('xdxf').first.attributes['lang_to'].value
+          )
       
-        # Articles
-        articles = doc.css('ar')
-        articles.each_with_index do |article, index|
-          (article_record = dictionary_record.articles.create!(
-            :the_article => article.text_or_inner_html,
-            :raw_text => article.serialize(serialize_options)
-          )).article_keys = article.css('k').map do |article_key|
-            XDXF::ArticleKey.find_or_create_by_the_key(
-              :the_key  => article_key.content,
-              :raw_text => article_key.serialize(serialize_options)
-            )
+          # Articles
+          articles = doc.css('ar')
+          articles.each_with_index do |article, index|
+            article_progress = "[#{index + 1}/#{articles.size}] (#{(index + 1)*100/articles.size}%)"
+            Article.benchmark("#{article_progress} articles import", Logger::DEBUG, false) do
+              article_record = dictionary_record.articles.create!(
+                :the_article => article.text_or_inner_html,
+                :raw_text => article.serialize(serialize_options)
+              )
+              article_record.article_keys = article.css('k').map do |article_key|
+                ArticleKey.find_or_create_by_the_key(
+                  :the_key  => article_key.content,
+                  :raw_text => article_key.serialize(serialize_options)
+                )
+              end
+
+              # Logging the article
+              article_info = "article keys: #{article_record.article_keys.map{|ak| ak.the_key}.join(",")}"
+              Article.logger.info article_info
+              puts article_info if options[:verbose]
+            end
           end
-          puts "<< [#{index + 1}/#{articles.size}] (#{(index + 1)*100/articles.size}%) #{article_record.article_keys.map{|ak| ak.the_key}.join(",")}" if options[:verbose]
+
+          # Logging the dictionary
+          dictionary_info = "#{dictionary_record.articles.count} of #{articles.size} articles created from dictionary '#{dictionary_record.full_name}'"
+          Dictionary.logger.info dictionary_info
+          puts dictionary_info if options[:verbose]
         end
 
-        puts "#{dictionary_record.articles.count} of #{articles.size} articles created from dictionary '#{dictionary_record.full_name}'" if options[:verbose]
-
-      end
+      end # transaction
      
       dictionary_record
     end
